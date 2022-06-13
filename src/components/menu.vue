31b7753eefec0963d1d929896b05f1ced6c8b9d9
tutubunso <aymardbutungane@gmail.com>
2022-06-13 18:48:28 +0200

    adding css code

---
+++ src/components/menu.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <div class="main">
+      <div class="menu">
+        <a href="#">Home</a>
+        <a href="#">Voitures</a>
+        <a href="#">Chauffeur</a>
+        <a href="#">Tarif</a>
+        <a href="#">Location</a>
+        <a href="#">LogOut</a>
+      </div>
+    </div>
+    </div>
+</template>
+<script> 
+    export default{
+        components:{
+
+        },
+        methods:{
+
+
+        }
+    };
+</script>
+<style scoped src="../../public/style.css">
+<style>
