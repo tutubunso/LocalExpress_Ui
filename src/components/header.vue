f9eb6542c1a0d42c5b8956276d7e0d431dd0f67d
tutubunso <aymardbutungane@gmail.com>
2022-06-13 18:48:18 +0200

    adding html code

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="entete">
+            <div class="hede">
+                <h1>LOCAREXPRESS COMPANY</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script> 
+    export default{
+        components:{
+
+        },
+        methods:{
+
+
+        }
+    };
+</script>
+<style scoped src="../../public/css/style.css">
+<style>
